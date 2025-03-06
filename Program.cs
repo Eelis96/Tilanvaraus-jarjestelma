6c0ca6b8ca2fb87496d9306dd5dcb1d33c3fc62a
Eelis96 <eelis.lantinen@gmail.com>
2025-03-06 10:50:27 +0200

    c# backend

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using StudentManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StudentContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+app.Run();
